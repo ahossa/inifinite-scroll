023fdca1ea97f98f03710acca816970f8eb0ba67
Asif Bin Hossain <a.evan17@gmail.com>
2020-07-24 17:45:33 -0400

    Fetching Data from Unsplash API + Hiding API Key

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("testing");
+// Unsplash API
+const count = 10;
+const apiKey = config.API_KEY;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get Photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On Load
+getPhotos();
